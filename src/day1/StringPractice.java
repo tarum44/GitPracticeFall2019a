5bd0322a02d624d6d95de8ff024be9ff9311725a
cybertekschool <github@cybertekschool.com>
2020-02-29 15:24:59 -0500

    modified StringPractice class

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -4,5 +4,7 @@ public class StringPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        String str = "Java is fun!";
+        System.out.println(str);
     }
 }
