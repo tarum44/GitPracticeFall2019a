eda68736abbe154a57d5f1af8d314a5d96831fee
muratkilinc <tarum44@gmail.com>
2020-03-11 19:15:40 -0500

    update

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -1,5 +1,5 @@
 public class Colors {
     public static void main(String[] args) {
-        String [] colors={"red","blue","green","yellow"};
+        String [] colors={"red","blue","green","yellow","orange"};
     }
 }
